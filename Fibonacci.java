378bd83ea8c48038fbc589b427e9a0e827e1be92
Quintin <quintinjohnson22@gmail.com>
2026-01-18 22:27:15 -0500

    Fixes #1: Add Javadoc comments to Fibonacci method

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,13 @@
+/**
+ * Defines a recursive method to calculate the Fibonacci sequence
+ */
 public class Fibonacci {
-
-
+    /**
+     * Returns the nth term in the Fibonacci sequence using recursion.
+     *
+     * @param n the position in the Fibonacci sequence
+     * @return the nth Fibonacci number
+     */
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
